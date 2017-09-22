408701e126030e8b31a67ace3a5ff30bea116617
Jakub Skierbiszewski <jakub.skierbiszewski@neofonie.de>
2017-09-22 14:29:46 +0200

    Improved API. Added tests. Fixed #1.

--- bundle-helper/src/main/java/com/jskierbi/bundle_helper/BundleExtrasExtensions.kt
+++ bundle-helper/src/main/java/com/jskierbi/bundle_helper/BundleExtrasExtensions.kt
@@ -1,32 +1,31 @@
 package com.jskierbi.bundle_helper
 
 import android.app.Activity
+import android.app.Fragment
 import android.content.Intent
 import android.os.Bundle
 import android.os.IBinder
 import android.os.Parcelable
 import android.os.PersistableBundle
+import android.support.v4.app.BundleCompat.putBinder
 import android.util.Log
 import android.util.Size
 import android.util.SizeF
 import java.io.Serializable
 
-//// ACTIVITY HELPERS /////
-/** Start activity with given extras */
-inline fun <reified T : Any> Activity.startActivity(vararg extras: Pair<String, Any?> = emptyArray()) {
-  val intent = Intent(this, T::class.java)
-  bundleFrom(extras)?.apply { intent.putExtras(this) }
-  startActivity(intent)
-}
+/** Lazy initialize with intent extra [key] */
+inline fun <reified T : Any?> Activity.lazyExtra(key: String)
+  = lazy { intent.extras?.get(key) as T }
 
 /** Lazy initialize with intent extra [key] */
-inline fun <reified T : Any?> Activity.lazyExtra(key: String) = lazy {
-  intent.extras?.get(key) as T
-}
+inline fun <reified T : Any?> android.support.v4.app.Fragment.lazyArg(key: String)
+  = lazy { arguments?.get(key) as T }
 
+/** Lazy initialize with intent extra [key] */
+inline fun <reified T : Any?> android.app.Fragment.lazyArg(key: String)
+  = lazy { arguments?.get(key) as T }
 
-//// FRAGMENT HELPERS /////
-/** Start activity from support fragment with given extras */
+/** Start activity from fragment with given extras */
 inline fun <reified T : Any> android.support.v4.app.Fragment.startActivity(vararg extras: Pair<String, Any?> = emptyArray()) {
   activity.startActivity<T>(*extras)
 }
@@ -36,69 +35,89 @@ inline fun <reified T : Any> android.app.Fragment.startActivity(vararg extras: P
   activity.startActivity<T>(*extras)
 }
 
+/** Start activity with given extras */
+inline fun <reified T : Any> Activity.startActivity(vararg extras: Pair<String, Any?> = emptyArray()) {
+  val intent = Intent(this, T::class.java)
+  intent.putExtras(Bundle().putExtras(extras))
+  startActivity(intent)
+}
+
+/** Put extras inside intent */
+fun Intent.withExtras(vararg extras: Pair<String, Any?>): Intent {
+  val bundle = Bundle()
+  bundle.putExtras(extras)
+  putExtras(bundle)
+  return this
+}
+
+/** Put args inside fragment. Reuses existing fragments arguments if set. */
+fun <T : Fragment> T.withArgs(vararg args: Pair<String, Any?> = emptyArray()): T {
+  val bundle = arguments ?: Bundle()
+  arguments = bundle
+  bundle.putExtras(args)
+  return this
+}
+
+/** Put args inside fragment. Reuses existing fragments arguments if set. */
+fun <T: android.support.v4.app.Fragment> T.withArgs(vararg args: Pair<String, Any?> = emptyArray()): T {
+  val bundle = arguments ?: Bundle()
+  arguments = bundle
+  bundle.putExtras(args)
+  return this
+}
+
+/** Create bundle from [extras] */
+fun Bundle.putExtras(extras: Array<out Pair<String, Any?>> = emptyArray()) = apply {
+  for ((key, value) in extras) {
+    when (value) {
+      null -> Unit // poor mans null safety
+      is IBinder -> putBinder(key, value)
+      is Boolean -> putBoolean(key, value)
+      is Bundle -> putBundle(key, value)
+      is Byte -> putByte(key, value)
+      is Char -> putChar(key, value)
+      is CharSequence -> putCharSequence(key, value)
+      is Double -> putDouble(key, value)
+      is Float -> putFloat(key, value)
+      is Int -> putInt(key, value)
+      is Long -> putLong(key, value)
+      is Parcelable -> putParcelable(key, value)
+      is Short -> putShort(key, value)
+      is Size -> putSize(key, value)
+      is SizeF -> putSizeF(key, value)
+      is String -> putString(key, value)
+      is Serializable -> {
+        Log.w("BundleHelperExtensionss.kt", "Warning: using Serializable for bundling value of class ${value.javaClass}")
+        putSerializable(key, value)
+      }
+      is PersistableBundle -> putAll(value)
+      else -> throw IllegalArgumentException("Cannot put to bundle, unsupported type: ${value.javaClass}")
+    }
+  }
+}
+
 /** Create support fragment with given args */
+@Deprecated("Please use Fragment#withArgs() func instead")
 inline fun <reified T : android.support.v4.app.Fragment> createSupportFragment(vararg args: Pair<String, Any?> = emptyArray()): T {
   val instance = try {
     T::class.java.newInstance()
   } catch (e: InstantiationException) {
     throw InstantiationException("Fragment ${T::class.simpleName} does not ")
   }
-  bundleFrom(args)?.apply { instance.arguments = this }
+  instance.arguments = Bundle().putExtras(args)
   return instance
 }
 
 /** Create fragment with given args */
+@Deprecated("Please use Fragment#withArgs() func instead")
 inline fun <reified T : android.app.Fragment> createFragment(vararg args: Pair<String, Any?> = emptyArray()): T {
   val instance = try {
     T::class.java.newInstance()
   } catch (e: InstantiationException) {
     throw InstantiationException("Fragment ${T::class.simpleName} does not ")
   }
-  bundleFrom(args)?.apply { instance.arguments = this }
+  instance.arguments = Bundle().putExtras(args)
   return instance
 }
 
-/** Lazy initialize with intent extra [key] */
-inline fun <reified T : Any?> android.support.v4.app.Fragment.lazyArg(key: String) = lazy {
-  arguments?.get(key) as T
-}
 
-/** Lazy initialize with intent extra [key] */
-inline fun <reified T : Any?> android.app.Fragment.lazyArg(key: String) = lazy {
-  arguments?.get(key) as T
-}
-
-/** Create bundle from [extras] */
-fun bundleFrom(extras: Array<out Pair<String, Any?>> = emptyArray()): Bundle? {
-  if (extras.isNotEmpty()) {
-    val bundle = Bundle()
-    for ((key, value) in extras) {
-      when (value) {
-        is IBinder -> bundle.putBinder(key, value)
-        is Boolean -> bundle.putBoolean(key, value)
-        is Bundle -> bundle.putBundle(key, value)
-        is Byte -> bundle.putByte(key, value)
-        is Char -> bundle.putChar(key, value)
-        is CharSequence -> bundle.putCharSequence(key, value)
-        is Double -> bundle.putDouble(key, value)
-        is Float -> bundle.putFloat(key, value)
-        is Int -> bundle.putInt(key, value)
-        is Long -> bundle.putLong(key, value)
-        is Parcelable -> bundle.putParcelable(key, value)
-        is PersistableBundle -> bundle.putAll(value)
-        is Short -> bundle.putShort(key, value)
-        is Size -> bundle.putSize(key, value)
-        is SizeF -> bundle.putSizeF(key, value)
-        is String -> bundle.putString(key, value)
-        is Serializable -> {
-          Log.w("BundleHelperExtensionss.kt", "Warning: using Serializable for bundling value of class ${value.javaClass}")
-          bundle.putSerializable(key, value)
-        }
-        null -> null // poor mans null safety
-        else -> throw IllegalArgumentException("Cannot put to bundle, unsupported type: ${value.javaClass}")
-      }
-    }
-    return bundle
-  }
-  return null
-}
