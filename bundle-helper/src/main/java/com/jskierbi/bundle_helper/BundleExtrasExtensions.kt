d701c03302d85a7f647d834df21d1370fe9057cf
Maciej Czerniak <maciek.czerniak@looksoft.pl>
2018-08-14 15:07:38 +0200

    fix issue on old android versions, update gradle, add on device tests

--- bundle-helper/src/main/java/com/jskierbi/bundle_helper/BundleExtrasExtensions.kt
+++ bundle-helper/src/main/java/com/jskierbi/bundle_helper/BundleExtrasExtensions.kt
@@ -3,10 +3,7 @@ package com.jskierbi.bundle_helper
 import android.app.Activity
 import android.app.Fragment
 import android.content.Intent
-import android.os.Bundle
-import android.os.IBinder
-import android.os.Parcelable
-import android.os.PersistableBundle
+import android.os.*
 import android.support.v4.app.BundleCompat.putBinder
 import android.util.Log
 import android.util.Size
@@ -69,28 +66,34 @@ fun <T: android.support.v4.app.Fragment> T.withArgs(vararg args: Pair<String, An
 /** Create bundle from [extras] */
 fun Bundle.putExtras(extras: Array<out Pair<String, Any?>> = emptyArray()) = apply {
   for ((key, value) in extras) {
-    when (value) {
-      null -> Unit // poor mans null safety
-      is IBinder -> putBinder(key, value)
-      is Boolean -> putBoolean(key, value)
-      is Bundle -> putBundle(key, value)
-      is Byte -> putByte(key, value)
-      is Char -> putChar(key, value)
-      is CharSequence -> putCharSequence(key, value)
-      is Double -> putDouble(key, value)
-      is Float -> putFloat(key, value)
-      is Int -> putInt(key, value)
-      is Long -> putLong(key, value)
-      is Parcelable -> putParcelable(key, value)
-      is Short -> putShort(key, value)
-      is Size -> putSize(key, value)
-      is SizeF -> putSizeF(key, value)
-      is String -> putString(key, value)
-      is Serializable -> {
-        Log.w("BundleHelperExtensionss.kt", "Warning: using Serializable for bundling value of class ${value.javaClass}")
+    when {
+      value == null -> Unit // poor mans null safety
+      value is IBinder -> {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR2) {
+          putBinder(key, value)
+        } else {
+          throw RuntimeException("IBinder is not supported on api level < 18")
+        }
+      }
+      value is Boolean -> putBoolean(key, value)
+      value is Bundle -> putBundle(key, value)
+      value is Byte -> putByte(key, value)
+      value is Char -> putChar(key, value)
+      value is CharSequence -> putCharSequence(key, value)
+      value is Double -> putDouble(key, value)
+      value is Float -> putFloat(key, value)
+      value is Int -> putInt(key, value)
+      value is Long -> putLong(key, value)
+      value is Parcelable -> putParcelable(key, value)
+      value is Short -> putShort(key, value)
+      BuildConfig.VERSION_CODE >= 21 && value is Size -> putSize(key, value)
+      BuildConfig.VERSION_CODE >= 21 && value is SizeF -> putSizeF(key, value)
+      value is String -> putString(key, value)
+      value is Serializable -> {
+        Log.w("BundleHelperExtensions", "Warning: using Serializable for bundling value of class ${value.javaClass}")
         putSerializable(key, value)
       }
-      is PersistableBundle -> putAll(value)
+      value is PersistableBundle -> putAll(value)
       else -> throw IllegalArgumentException("Cannot put to bundle, unsupported type: ${value.javaClass}")
     }
   }
